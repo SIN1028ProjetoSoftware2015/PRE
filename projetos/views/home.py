ce193e976e711d623c35ae16865b86dc0b3fda1c
AngelicaSkalee <angiiskalee@gmail.com>
2015-10-22 00:02:01 -0200

    filtros na tela inicial

--- projetos/views/home.py
+++ projetos/views/home.py
@@ -5,27 +5,46 @@ import datetime
 
 # Create your views here.
 class HomeAdmin(TemplateView):
-    template_name = 'projetos/home.html'
+	template_name = 'projetos/home.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(HomeAdmin, self).get_context_data(**kwargs)
-        context['menu'] = 'home'
-        context['total_participantes'] = Participante.objects.count()
-        context['total_projetos'] = Projeto.objects.count()
-        context['legend_template'] = "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-        context['projeto_situacao'] = get_situacao_projetos()
-        context['vinculo_participantes'] = get_vinculo_participantes()
-        context['projeto_departamento'] = get_projeto_departamento()
-        context['projeto_unidade'] = get_projeto_unidade()
-        context['participante_unidade'] = get_participante_unidade()
-        context['projetos_por_ano'] = get_projetos_por_ano()
-        return context
+	def get_context_data(self, **kwargs):
+		context = super(HomeAdmin, self).get_context_data(**kwargs)
+		context['menu'] = 'home'
+		context['total_participantes'] = Participante.objects.count()
+		context['total_projetos'] = Projeto.objects.count()
+		filtros = None
+		if 'params' in kwargs:
+			filtros = kwargs['params']
+		if filtros!=None and len(filtros)>0:
+			for k, v in filtros.items():
+				if 'filter_year1' in k or 'filter_year2' in k:
+					context['filtro_nome'] = k
+					context['filtro_valor'] = v
+		context['legend_template'] = "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+		context['projeto_situacao'] = get_situacao_projetos(filtros)
+		context['vinculo_participantes'] = get_vinculo_participantes(filtros)
+		context['projeto_departamento'] = get_projeto_departamento()
+		context['projeto_unidade'] = get_projeto_unidade()
+		context['participante_unidade'] = get_participante_unidade()
+		context['projetos_por_ano'] = get_projetos_por_ano()
+		return context
 
+	def post(self, request, *args, **kwargs):
+		if request.method == 'POST':
+			request.POST._mutable = True
+			params = {'params': dict((p, v) for p, v in request.POST.items() if v != None and len(v) > 0)}
+			return self.render_to_response(self.get_context_data(**params))
 
-def get_situacao_projetos():
+def get_situacao_projetos(filtros):
+	print("AQUII")
 	mapa = {}
-	for m in Projeto.objects.order_by().values('situacao').distinct():
-		mapa[m['situacao']] = Projeto.objects.filter(situacao=m['situacao']).count()
+	if filtros and 'filter_year1' in filtros and filtros['filter_year1'] != 'Todos':
+		for m in Projeto.objects.order_by().values('situacao').distinct():
+			mapa[m['situacao']] = Projeto.objects.filter(situacao=m['situacao'], data_registro__year=filtros['filter_year1']).count()
+			#print(Projeto.objects.filter(situacao=m['situacao'], data_registro__year=filtros['filter_year1']).query)
+	else:
+		for m in Projeto.objects.order_by().values('situacao').distinct():
+			mapa[m['situacao']] = Projeto.objects.filter(situacao=m['situacao']).count()
 	return mapa
 
 def get_projetos_por_ano():
@@ -39,10 +58,14 @@ def get_projetos_por_ano():
 	return mapa
 
 
-def get_vinculo_participantes():
+def get_vinculo_participantes(filtros):
 	mapa = {}
-	for m in ProjetoParticipante.objects.order_by().values('vinculo').distinct():
-		mapa[m['vinculo']] = ProjetoParticipante.objects.filter(vinculo=m['vinculo']).values('participante').distinct().count()
+	if filtros and 'filter_year2' in filtros and filtros['filter_year2'] != 'Todos':
+		for m in ProjetoParticipante.objects.order_by().values('vinculo').distinct():
+			mapa[m['vinculo']] = ProjetoParticipante.objects.filter(vinculo=m['vinculo'], data_inicial__year=filtros['filter_year2']).values('participante').distinct().count()
+	else:
+		for m in ProjetoParticipante.objects.order_by().values('vinculo').distinct():
+			mapa[m['vinculo']] = ProjetoParticipante.objects.filter(vinculo=m['vinculo']).values('participante').distinct().count()
 	return mapa
 
 def get_projeto_departamento():
