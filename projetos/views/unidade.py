19b20429bb0125e6801e597e4558530c4b9e8cff
AngelicaSkalee <angiiskalee@gmail.com>
2015-11-24 19:37:37 -0200

    alteracao nos detalhes do sistema

--- projetos/views/unidade.py
+++ projetos/views/unidade.py
@@ -2,8 +2,6 @@ from django.views.generic import TemplateView
 from projetos.models import Projeto, Participante, ProjetoParticipante, Unidade, ProjetoUnidade, Departamento
 from projetos import utils
 import datetime
-
-# Create your views here.
 class GraficosAdmin(TemplateView):
 	template_name = 'projetos/graficos.html'
 
@@ -33,36 +31,9 @@ class GraficosAdmin(TemplateView):
 			request.POST._mutable = True
 			params = {'params': dict((p, v) for p, v in request.POST.items() if v != None and len(v) > 0)}
 			return self.render_to_response(self.get_context_data(**params))
-
-def get_projeto_departamento(filtros):
-	mapa = {}
-	if filtros:
-		for m in Departamento.objects.filter(codigo__in=filtros).order_by('nome'):
-			mapa[m.nome] = {}
-			mapa[m.nome]['count'] = Projeto.objects.filter(departamento_id=m.codigo).count()
-			mapa[m.nome]['id'] = m.codigo
-	else:
-		for m in Departamento.objects.all().order_by('nome'):
-			mapa[m.nome] = {}
-			mapa[m.nome]['count'] = Projeto.objects.filter(departamento_id=m.codigo).count()
-			mapa[m.nome]['id'] = m.codigo
-	return mapa
-
-def get_projeto_departamento_todos():
-	mapa = {}
-	for m in Departamento.objects.all():
-		mapa[m.nome] = {}
-		mapa[m.nome]['id'] = str(m.codigo)
-	return mapa
-
+			
 def get_projeto_unidade():
 	mapa = {}
 	for m in Unidade.objects.all():
 		mapa[m.nome] = ProjetoUnidade.objects.filter(unidade=m.codigo).count()
-	return mapa
-
-def get_participante_unidade():
-	mapa = {}
-	for m in Unidade.objects.all():
-		mapa[m.nome] = Participante.objects.filter(unidade=m.codigo).count()
 	return mapa
